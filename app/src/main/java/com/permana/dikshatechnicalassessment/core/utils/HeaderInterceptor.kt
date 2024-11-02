159a2313ee971dd4352ef5727a096f927b111e1a
Permana <permana4131@gmail.com>
2024-11-02 21:33:26 +0700

    update

--- app/src/main/java/com/permana/dikshatechnicalassessment/core/utils/HeaderInterceptor.kt
+++ app/src/main/java/com/permana/dikshatechnicalassessment/core/utils/HeaderInterceptor.kt
@@ -4,7 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class HeaderInterceptor: Interceptor {
-    val apiKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzRiMWNhNzNlZGQ4MDc2YWJiY2Y4ODFhMGJmZDU3ZiIsIm5iZiI6MTczMDM3MDkyMy44MTM5ODQ0LCJzdWIiOiI2MGIwODlkNTUyZGM3ZjAwNTc5MzVmYzciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fIf_hC5C8NT7bDoJg9j5tcfMGVPSTK33HV7bhiY-qoI"
+    val apiKey = "token if any"
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val requestBuilder = request.newBuilder()
